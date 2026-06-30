14ec5f43186afb42defa5679a95aaab16171c79d
Ayoub Gutin <gutinayoub@gmail.com>
2026-06-30 12:33:28 +0100

    expected duration and forward pass done

--- engine.py
+++ engine.py
@@ -2,7 +2,7 @@ from models import TaskInput
 from typing import List
 
 
-def transform_inputs(task_list: List[TaskInput]):
+def transform_inputs(task_list: List[TaskInput]) -> dict:
     """
     Takes a list of tasks, that adhere to the TaskInput schema.
 
@@ -31,3 +31,39 @@ def transform_inputs(task_list: List[TaskInput]):
         }
 
     return processed_tasks
+
+
+def calc_forward_pass(tasks: dict) -> float:
+    """
+    Calcs the forward pass (ES+EF) for all tasks in the network
+
+    Returns the absolute project duration
+    """
+    visited = set()
+
+    def process_task(t_id: str):
+        if t_id in visited or t_id not in tasks:
+            return
+
+        task = tasks[t_id]
+
+        for pred in task["predecessors"]:
+            if pred in tasks and pred not in visited:
+                process_task(pred)
+
+        if not task["predecessors"]:
+            task["es"] = float(0)
+        else:
+            valid_preds = [p for p in task["predecessors"] if p in tasks]
+            tasks["es"] = (
+                max(tasks[p]["ef"] for p in valid_preds) if valid_preds else float(0)
+            )
+
+        task["ef"] = round(task["es"] + task["duration"], 2)
+        visited.add(t_id)
+
+    for t_id in list(tasks.keys()):
+        process_task(t_id)
+
+    project_duration = max([t["ef"] for t in tasks.values()]) if tasks else 0.0
+    return round(project_duration, 2)
